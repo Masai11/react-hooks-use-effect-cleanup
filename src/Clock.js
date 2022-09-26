b3c2f3219a2e58ff4a84d782b2702345a62bda22
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-09-26 17:01:22 +0300

    completed lesson

--- src/Clock.js
+++ src/Clock.js
@@ -4,10 +4,17 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+   const timerID =  setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // RETURNING A CLEANUP FUNCTION
+    return function cleanup() {
+      clearInterval(timerID)
+    };
   }, []);
+
+
 
   return <div>{time.toString()}</div>;
 }
